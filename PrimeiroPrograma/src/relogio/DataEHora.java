69aef34746fc6f2cfba1668480382419d1203c78
Marcio J. Costa <87935294+MarcioCosta013@users.noreply.github.com>
2023-09-27 19:18:40 -0300

    1° Commit de atividades Java

---
+++ PrimeiroPrograma/src/relogio/DataEHora.java
@@ -0,0 +1,16 @@
+package relogio;
+
+import java.util.Date;
+
+public class DataEHora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date datahora = new Date();
+		System.out.println("A hora do Sistema desse PC é:");
+		System.out.println(datahora.toString());
+
+		//como pegar a data e hora do sistema
+	}
+
+}
