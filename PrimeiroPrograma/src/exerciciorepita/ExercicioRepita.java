6af4af8a0e0e0de6ec5dc89f3b2959b3d5460c7e
Marcio Costa <marcio-costa38@hotmail.com>
2023-10-21 14:23:18 -0300

    Fiz uma tela automatica com o JOptionPane que soma todos os numeros.

---
+++ PrimeiroPrograma/src/exerciciorepita/ExercicioRepita.java
@@ -0,0 +1,52 @@
+package exerciciorepita;
+
+import javax.swing.JOptionPane; //Tem que importar essa classe para ter telas automaticamente...
+
+public class ExercicioRepita {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//JOptionPane.showMessageDialog(null, "Olá Mundo", "Boas Vindas", JOptionPane.INFORMATION_MESSAGE); (para mstrar menssagens...
+		int digi = 0, numPar = 0, numImpar = 0, maiorQue =0;
+		double valor, resul = 0;
+		double media;
+		String mediaf;
+		
+		do {
+			
+			valor = Double.parseDouble(JOptionPane.showInputDialog(null, "Digite um numero a ser somado: (digite zero para terminar)"));//( para pegar valores digitados)
+			
+			if (valor != 0) {
+				digi++;
+			}
+				
+			
+			if ((valor % 2) == 0) { //Para verificar sew é impar ou par.
+				if (valor != 0) {
+					numPar++;
+				}
+				
+			} else {
+				numImpar++;
+				
+			}
+			
+			if (valor > 100) { //para contar os numeros maiores que 100.
+				maiorQue++;
+			}
+			
+			resul += valor; //Para somar os novos valores digitados.
+			
+			media = (double) (resul/digi);
+			
+		} while ( valor != 0);
+		
+		JOptionPane.showMessageDialog(null, "<html>Total de numeros digitados: " + digi + "<br>Resultado final da soma é: " + resul + "<br>Total de Numeros par: " + numPar + "<br>Total de Numeros Impares: "
+										+ numImpar + "<br>Acima de 100: " + maiorQue + "<br>Media dos valores digitados:" + media + "</html>");
+		
+		
+
+	}
+
+}
